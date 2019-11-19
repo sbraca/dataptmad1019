8211adbdb141e6245723633a25748b58adcb9162
Stefania Braca <stefaniabraca@gmail.com>
2019-11-19 18:34:42 +0100

    Challenge-2

--- module-1/lab-mysql-select/your-code/Solutions.sql
+++ module-1/lab-mysql-select/your-code/Solutions.sql
@@ -4,18 +4,18 @@ join titleauthor on titles.title_id = titleauthor.title_id
 join authors on authors.au_id = titleauthor.au_id
 join publishers on titles.pub_id = publishers.pub_id*/
 
-/*#challenge 2
-SELECT authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name
-Select Count (titles.title_id)
-from titles 
-group by titleauthor.au_id;*/
 
-SELECT authors.au_id, authors.au_lname, authors.au_fname, sales.qty
+/*SELECT 
+authors.au_id AS 'AUTHOR ID'
+authors.au_lname AS 'LAST NAME'
+authors.au_fname AS 'FIRST NAME'
+publisher.pub_name AS 'PUBLISHER'
+COUNT(titles.title_id) AS 'TITLE COUNT'
 FROM titles 
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on authors.au_id = titleauthor.au_id
-Count (sales.title_id)
-order by titleauthor.au_id
+JOIN publisher ON publisher.title_id = titles.title_id
+GROUP BY authors.au_id;*/
 
 
 
