362326428b00f629c828727a3940937732d6a1e8
Stefania Braca <stefaniabraca@gmail.com>
2019-11-19 22:52:23 +0100

    lab finished

--- module-1/lab-mysql-select/your-code/Solutions.sql
+++ module-1/lab-mysql-select/your-code/Solutions.sql
@@ -1,21 +1,60 @@
-/*SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
+
+
+/*SELECT
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+titles.title AS 'TITLES',
+publishers.pub_name AS 'PUBLISHER' 
 from titles
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on authors.au_id = titleauthor.au_id
-join publishers on titles.pub_id = publishers.pub_id*/
+join publishers on titles.pub_id = publishers.pub_id;*/
 
-
-/*SELECT 
-authors.au_id AS 'AUTHOR ID'
-authors.au_lname AS 'LAST NAME'
-authors.au_fname AS 'FIRST NAME'
-publisher.pub_name AS 'PUBLISHER'
+/*SELECT
+SUM ('TITLE COUNT')
+FROM
+(
+SELECT 
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+publishers.pub_name AS 'PUBLISHER',
 COUNT(titles.title_id) AS 'TITLE COUNT'
 FROM titles 
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on authors.au_id = titleauthor.au_id
-JOIN publisher ON publisher.title_id = titles.title_id
-GROUP BY authors.au_id;*/
+JOIN publishers ON publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id, publishers.pub_id
+Order by authors.au_id DESC
+);*/
+
+
+
+/*SELECT 
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+Count(sales.qty) AS 'TOTAL TITLES'
+FROM titles 
+join titleauthor on titles.title_id = titleauthor.title_id
+join authors on authors.au_id = titleauthor.au_id
+join sales on sales.title_id = titles.title_id
+GROUP BY sales.title_id
+ORDER BY Count(sales.qty) DESC
+limit 3;*/
+
+/*SELECT 
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+Count(sales.qty) AS 'TOTAL TITLES'
+FROM authors 
+left join titleauthor on authors.au_id = titleauthor.au_id
+left join titles on titles.title_id = titleauthor.title_id
+left join sales on sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY Count(sales.qty) DESC;*/
 
 
 
